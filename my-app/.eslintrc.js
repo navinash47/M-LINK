be03aca822a4eeca6efc91a353ed8345aeb39467
navinash47 <navinash47@gmail.com>
2024-01-30 19:54:29 +0530

    added expo files with react native cli

---
+++ my-app/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "@react-native-community"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        semi: ['error', 'never'],
+        'comma-dangle': [2, 'never'],
+        'react/jsx-filename-extension': [1, { extension: ['.js ','.jsx']}],
+        'no-use-before-define': [
+            'error',
+            {functions: true, classes: true, variables: true}
+        ]
+
+    }
+}
